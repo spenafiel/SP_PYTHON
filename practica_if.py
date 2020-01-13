e0f77cd3d8f86d031e2995148f3137341f771c81
spenafiel <59834728+spenafiel@users.noreply.github.com>
2020-01-13 10:31:41 -0500

    Add files via upload

---
+++ practica_if.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 12:18:29 2020
+
+@author: CEC
+"""
+
+nativeVLAN = 100
+dtaVLAN =100
+if nativeVLAN == dtaVLAN:
+    print("Verdadero")
+else:
+    print("Falso")

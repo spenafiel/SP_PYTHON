e61df2f86817abc733054537b807caf7a5f1352d
spenafiel <59834728+spenafiel@users.noreply.github.com>
2020-01-13 10:32:00 -0500

    Add files via upload

---
+++ practica3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 11:57:48 2020
+
+@author: CEC
+"""
+
+a1 = input("Hola Bienvenido al curso python:")
+a2 = input("Cual es tu nombre:")
+a3 = input("Cual es tu edad:")
+a4 = input("Email:")
+space =" "
+print("Saludo:" + a1 + " Tu nombre es:" + a2 + "tu Edad es:" + a3 + "Tu email es:" + a4)
+
+a5 = [input("Hola Bienvenido al curso python:"), input("Cual es tu nombre:"), input("Cual es tu edad:"), input("Email:")
+]
+
+print(a5)
